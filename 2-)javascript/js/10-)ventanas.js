24783fa7d7e78f4c5eb13546bde1c68026f87fdc
John Olivares <olivaresvjohn@gmail.com>
2021-05-27 17:00:56 -0400

    Alertas y Ventanas

---
+++ 2-)javascript/js/10-)ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Aletrta
+alert('Esta es mi alerta');
+
+//Alerta de Confirmacion
+var resultado = confirm('Estas seguro de querer continuar?');
+console.log(resultado);
+
+//Ventana de ingreso de Informacion
+var edad = prompt('Que edad tienes?', 18);
+console.log(typeof edad);
